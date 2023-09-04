f4297ba3c6935d7e2a109034c6e014945957cdc5
seonghoo1217 <seonghoo1217@naver.com>
2023-09-05 08:59:43 +0900

    # chore : initial commit

---
+++ tobi/src/test/java/com/example/tobi/TobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

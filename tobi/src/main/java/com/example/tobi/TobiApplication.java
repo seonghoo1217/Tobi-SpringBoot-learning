5d496df410fce8f40d6a4ac62929eaa0116e4972
seonghoo1217 <seonghoo1217@naver.com>
2023-09-07 16:51:44 +0900

    create new WebServer used by Tomcat

--- tobi/src/main/java/com/example/tobi/TobiApplication.java
+++ tobi/src/main/java/com/example/tobi/TobiApplication.java
@@ -1,12 +1,15 @@
 package com.example.tobi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class TobiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello  Containerless Standalone Application");
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }

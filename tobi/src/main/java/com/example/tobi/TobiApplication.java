33a0fa16b64c31b61775aca3eba547e8b5a31fa4
seonghoo1217 <seonghoo1217@naver.com>
2023-09-07 16:21:27 +0900

    eliminate main spring boot application

--- tobi/src/main/java/com/example/tobi/TobiApplication.java
+++ tobi/src/main/java/com/example/tobi/TobiApplication.java
@@ -3,11 +3,10 @@ package com.example.tobi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class TobiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TobiApplication.class, args);
+		System.out.println("Hello  Containerless Standalone Application");
 	}
 
 }

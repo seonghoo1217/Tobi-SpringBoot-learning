e57c1163016bf728dcedd171e51ef81270a395a9
seonghoo1217 <seonghoo1217@naver.com>
2023-09-08 01:26:43 +0900

    add front controller when separate http method type

--- tobi/src/main/java/com/example/tobi/TobiApplication.java
+++ tobi/src/main/java/com/example/tobi/TobiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,16 +19,26 @@ public class TobiApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					//인증 보안 다국어 처리등 공통적으로 동작할 수 있는 로직들
+					if (req.getRequestURI().equals("/hello")&&req.getMethod().equals(HttpMethod.GET.name())){
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello "+name);
+						String name = req.getParameter("name");
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello "+name);
+					}
+					else if (req.getRequestURI().equals("/user")){
+						//
+					}
+					else {
+						resp.setStatus(404);
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 	}

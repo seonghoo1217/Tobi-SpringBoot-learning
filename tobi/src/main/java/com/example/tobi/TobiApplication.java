9c7ffed4ce6252b512b3fdb81d6c82c6bad22e25
seonghoo1217 <seonghoo1217@naver.com>
2023-09-12 23:20:13 +0900

    context mapping use dispatch servlet

--- tobi/src/main/java/com/example/tobi/TobiApplication.java
+++ tobi/src/main/java/com/example/tobi/TobiApplication.java
@@ -1,55 +1,25 @@
 package com.example.tobi;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TobiApplication {
 
 	public static void main(String[] args) {
 
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//인증 보안 다국어 처리등 공통적으로 동작할 수 있는 로직들
-
-					// 매핑 : URL 패턴을 기반으로 처리할 컴포넌트를 지정해주는
-					// 바인딩: 직접적인 웹요청정보를 변환시켜 전달하는 것
-					if (req.getRequestURI().equals("/hello")&&req.getMethod().equals(HttpMethod.GET.name())){
-
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else {
-						resp.setStatus(404);
-					}
-				}
-			}).addMapping("/*");
+			servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(applicationContext))
+					.addMapping("/*");
 		});
 		webServer.start();
 	}

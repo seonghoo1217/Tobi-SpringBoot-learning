7089093d64782fa8e96c238a915aaa6cb0161c2b
seonghoo1217 <seonghoo1217@naver.com>
2023-09-14 01:25:47 +0900

    기존 어노테이션을 이용한 Bean등록 방식 -> 컴포넌트 스캔 기반의 아키텍쳐 분리

--- tobi/src/main/java/com/example/tobi/HelloController.java
+++ tobi/src/main/java/com/example/tobi/HelloController.java
@@ -3,10 +3,11 @@ package com.example.tobi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -14,10 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name){
-
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
